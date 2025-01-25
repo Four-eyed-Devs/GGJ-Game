0c1184f022a8197180be42f8c9bba8c5a16211be
Robert Varga <rvarga253@yahoo.com>
2025-01-25 23:24:34 +0200

    Finished boss and IFrames

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -5,14 +5,25 @@ using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
+    private SpriteRenderer sr;
+
     public float health; 
     public float maxHealth;
     public Slider PlayerHealthBar;
     public GameOverScreen GameOverScreen;
 
+    public Color flashColor;
+    public Color regularColor;
+    public float flashDuration;
+    public int numberOfFlashes;
+
+    private bool canTakeDamage = true;
+
     // Start is called before the first frame update
     void Start()
     {
+        sr = GetComponent<SpriteRenderer>();
+        
         maxHealth = health;
     }
 
@@ -24,12 +35,35 @@ public class PlayerHealth : MonoBehaviour
 
     public void TakeDamage(float damage)
     {
-        health -= damage;
-        if (health <= 0)
+        if(canTakeDamage)
         {
-            Destroy(gameObject);
-            GameOverScreen.Setup();
+            health -= damage;
+
+            StartCoroutine(IFrames());
+
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+                GameOverScreen.Setup();
+            }
         }
     }
 
+    private IEnumerator IFrames()
+    {
+        canTakeDamage = false;
+
+        int temp = 0;
+
+        while(temp < numberOfFlashes)
+        {
+            sr.color = flashColor;
+            yield return new WaitForSeconds(flashDuration);
+            sr.color = regularColor;
+            yield return new WaitForSeconds(flashDuration);
+            temp++;
+        }
+
+        canTakeDamage = true;
+    }
 }
